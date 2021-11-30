d594912107738a9ffa2cf23a2c5637d76704ca9c
Oleg Stepanov <olezhka106s@gmail.com>
2021-11-30 18:44:55 +0300

    Initial Commit

---
+++ CatsSwiftUI/CatsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsSwiftUIApp.swift
+//  CatsSwiftUI
+//
+//  Created by Oleg Stepanov on 30.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CatsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
